77bc33a99a6c79a24196e815c3e7cef5d855b604
vercte <me@vercte.net>
2026-09-13 19:09:54 -0700

    change weight

--- src/main/java/net/vercte/gleefulcreepers/util/datagen/GleefulDatagen.java
+++ src/main/java/net/vercte/gleefulcreepers/util/datagen/GleefulDatagen.java
@@ -61,7 +61,7 @@ public class GleefulDatagen {
                             )
                     );
 
-                    MobSpawnSettings.SpawnerData gleeperSpawnerData = new MobSpawnSettings.SpawnerData(GleefulCreepers.GLEEPER.get(), 4, 4, 100);
+                    MobSpawnSettings.SpawnerData gleeperSpawnerData = new MobSpawnSettings.SpawnerData(GleefulCreepers.GLEEPER.get(), 200, 4, 4);
                     bootstrap.register(ADD_GLEEPERS_TO_LUSH_CAVES,
                             new BiomeModifiers.AddSpawnsBiomeModifier(
                                     biomes.getOrThrow(GleefulTags.GLEEPER_SPAWNS_IN),
